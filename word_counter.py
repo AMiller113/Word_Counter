f4d7ebee2005c0e01988cf74e7aff1b4453c3b1d
Akeem Miller <ajayj46@gmail.com>
2020-09-02 12:46:08 -0400

    Added Several Function Definitions

--- word_counter.py
+++ word_counter.py
@@ -4,24 +4,39 @@ import re
 
 class WordCounter():
     def __init__(self, file_name):
-        self.file_name = file_name
+        self.file_name = '/content/drive/My Drive/Test Files/' + file_name
         self.tokens = []
+        self.words = []
         self.word_count_dict = {}
         self.total_word_count = 0
         self.total_token_count = 0
         self.unique_word_count = 0
+        nltk.download('punkt')
 
-    def get_word_count(self):
+    def get_word_count_dict(self):
         pass
 
     def txt_tokenizer(self):
-        pass
+        try:
+            file = open(self.file_name, mode='r')
+        except:
+            return False
+        else:
+            text = file.read()
+            self.tokens = nltk.word_tokenize(text)
+            return True
+
+    def get_words(self):
+        for token in self.tokens:
+            print(token)
+            if re.match('^[a-zA-Z0-9]+$', token):
+                self.words.append(token)
 
     def count_tokens(self):
-        pass
+        self.total_token_count = len(self.tokens)
 
     def count_words(self):
-        pass
+        self.total_word_count = len(self.words)
 
-    def validate_file_name(self):
-        pass
+    def count_unique_words(self):
+        self.unique_word_count = len(set(self.words))
