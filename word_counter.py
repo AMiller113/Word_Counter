bedbed757ea9075b98f4f7e41268fddce01e38e0
Akeem Miller <ajayj46@gmail.com>
2020-09-02 13:21:10 -0400

    Implemented function for to get word dictionary

--- word_counter.py
+++ word_counter.py
@@ -1,10 +1,12 @@
 import nltk
 import re
 
+# TODO find a way to remove tokens that are not actually words
+
 
 class WordCounter():
     def __init__(self, file_name):
-        self.file_name = '/content/drive/My Drive/Test Files/' + file_name
+        self.file_name = file_name
         self.tokens = []
         self.words = []
         self.word_count_dict = {}
@@ -14,7 +16,11 @@ class WordCounter():
         nltk.download('punkt')
 
     def get_word_count_dict(self):
-        pass
+        for word in self.words:
+            if word not in self.word_count_dict:
+                self.word_count_dict[word] = 1
+            else:
+                self.word_count_dict[word] += 1
 
     def txt_tokenizer(self):
         try:
@@ -28,7 +34,6 @@ class WordCounter():
 
     def get_words(self):
         for token in self.tokens:
-            print(token)
             if re.match('^[a-zA-Z0-9]+$', token):
                 self.words.append(token)
 
